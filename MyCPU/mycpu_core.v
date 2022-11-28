a6946c635a84e6c4b5677c5192f05254f716feb6
Mrrrrrrrrrrr <96118055+Mrrrrrrrrrrr@users.noreply.github.com>
2022-11-29 01:05:49 +0800

    完成RAW数据相关处理（除LOAD） (#5)

--- MyCPU/mycpu_core.v
+++ MyCPU/mycpu_core.v
@@ -1,4 +1,4 @@
-`include "lib/defines.vh"
+`include "defines.vh"
 module mycpu_core(
     input wire clk,
     input wire rst,
@@ -28,6 +28,8 @@ module mycpu_core(
     wire [`BR_WD-1:0] br_bus; 
     wire [`DATA_SRAM_WD-1:0] ex_dt_sram_bus;
     wire [`WB_TO_RF_WD-1:0] wb_to_rf_bus;
+    wire [`EX_TO_ID_WD-1:0] ex_to_id_bus;
+    wire [`MEM_TO_ID_WD-1:0] mem_to_id_bus;
     wire [`StallBus-1:0] stall;
 
     IF u_IF(
@@ -41,7 +43,6 @@ module mycpu_core(
         .inst_sram_addr  (inst_sram_addr  ),
         .inst_sram_wdata (inst_sram_wdata )
     );
-    
 
     ID u_ID(
     	.clk             (clk             ),
@@ -51,6 +52,8 @@ module mycpu_core(
         .if_to_id_bus    (if_to_id_bus    ),
         .inst_sram_rdata (inst_sram_rdata ),
         .wb_to_rf_bus    (wb_to_rf_bus    ),
+        .ex_to_id_bus    (ex_to_id_bus    ),
+        .mem_to_id_bus   (mem_to_id_bus   ),
         .id_to_ex_bus    (id_to_ex_bus    ),
         .br_bus          (br_bus          )
     );
@@ -61,6 +64,7 @@ module mycpu_core(
         .stall           (stall           ),
         .id_to_ex_bus    (id_to_ex_bus    ),
         .ex_to_mem_bus   (ex_to_mem_bus   ),
+        .ex_to_id_bus    (ex_to_id_bus    ),
         .data_sram_en    (data_sram_en    ),
         .data_sram_wen   (data_sram_wen   ),
         .data_sram_addr  (data_sram_addr  ),
@@ -73,7 +77,8 @@ module mycpu_core(
         .stall           (stall           ),
         .ex_to_mem_bus   (ex_to_mem_bus   ),
         .data_sram_rdata (data_sram_rdata ),
-        .mem_to_wb_bus   (mem_to_wb_bus   )
+        .mem_to_wb_bus   (mem_to_wb_bus   ),
+        .mem_to_id_bus   (mem_to_id_bus   )
     );
     
     WB u_WB(
